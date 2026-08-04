29b8b68a2ea9fbaa9987bb36cbf7ba4197c05d4d
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-08-04 14:32:02 +0800

    init project

---
+++ SpaceSlim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpaceSlim
+//
+//  Created by gaozhongkui on 2026/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpaceSlim/SpaceSlimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceSlimApp.swift
+//  SpaceSlim
+//
+//  Created by gaozhongkui on 2026/8/4.
+//
+
+import SwiftUI
+
+@main
+struct SpaceSlimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
